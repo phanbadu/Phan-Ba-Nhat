d0987c446f80181d14267aedb1c2007d484836cf
phanbadu <duphan03@gmail.com>
2024-03-18 06:12:36 +0700

    api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,31 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
+mongoose.connect(process.env.MONGO)
+    .then(() => console.log("MongoDb is connected"))
+    .catch((err) => console.log(err))
 
 app.listen(3000, () => {
-    console.log("oke");
+    console.log("Server is running on port 3000!");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
